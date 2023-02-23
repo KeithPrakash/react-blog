7e7f60b09d6656f7d92c9ab4a54f10d70dfa62a6
Keith Prakash <71426291+KeithPrakash@users.noreply.github.com>
2023-02-23 11:15:02 +0530

    insital commit

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Luna Blog</h1>
+            <a href="/">Home</a>
+            <a href="/create">Blog</a>
+        </nav>
+     );
+}
+ 
+export default Navbar;
